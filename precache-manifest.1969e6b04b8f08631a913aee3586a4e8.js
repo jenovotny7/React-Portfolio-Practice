3c1da274fa6d47980ef1e7552d50193ea3aa5d25
Jack Novotny <jenovotny7@gmail.com>
2021-03-15 18:18:48 -0500

    Updates

---
+++ precache-manifest.1969e6b04b8f08631a913aee3586a4e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4255a11ea6c37ad539af40be50b84d32",
+    "url": "/portfoliovandy/index.html"
+  },
+  {
+    "revision": "dd401d3c02ffe4111f86",
+    "url": "/portfoliovandy/static/css/main.159b819c.chunk.css"
+  },
+  {
+    "revision": "151d9e3a885d5c4d728b",
+    "url": "/portfoliovandy/static/js/2.e5ef0cf0.chunk.js"
+  },
+  {
+    "revision": "dd401d3c02ffe4111f86",
+    "url": "/portfoliovandy/static/js/main.645d4571.chunk.js"
+  },
+  {
+    "revision": "2b674046a56d6ed8672d",
+    "url": "/portfoliovandy/static/js/runtime~main.db3924d0.js"
+  },
+  {
+    "revision": "10cbe3553b6b46cc153ea61530dc0efa",
+    "url": "/portfoliovandy/static/media/Quiz.10cbe355.png"
+  },
+  {
+    "revision": "ec787e6ef40d6150bdf049bb1f861aee",
+    "url": "/portfoliovandy/static/media/Street.ec787e6e.png"
+  },
+  {
+    "revision": "5ceaa8bc0c762f6670598a5afa7f4dda",
+    "url": "/portfoliovandy/static/media/profile.5ceaa8bc.jpg"
+  },
+  {
+    "revision": "df68f09dc8c5cad3d3eb5108928ecd53",
+    "url": "/portfoliovandy/static/media/profile3.df68f09d.jpeg"
+  },
+  {
+    "revision": "4a6c67f0fe26e7bf768f4c71c61ee5f3",
+    "url": "/portfoliovandy/static/media/weather.4a6c67f0.png"
+  }
+]);
